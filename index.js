f35a1c30d791d9afff030e47f9363edbb2262f15
Alfred Shaheen <shaheenalfred48@gmail.com>
2022-10-13 09:09:06 -0700

    commit tree 8106dc875b99c7a62a253ce9d6714ccec722b2e8

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, event) { 
+    let cards = []
+    for (let i = 0; i < names.length; i++) {   
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+
+    }
+    return cards
+    
+}
+
+function countDown() {
+    let countDown = 0;
+    while (countDown <= 10) {
+        console.log(countDown++)
+ 
+    }
+}
